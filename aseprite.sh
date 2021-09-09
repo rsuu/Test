70d8b6095a949afccbee0269eec2d0b7b8209dc6
TK4E <tk4ecc@gmail.com>
2021-09-09 10:36:49 +0800

    2021-09-09

--- aseprite.sh
+++ aseprite.sh
@@ -1,20 +1,21 @@
-sudo mkdir ~/deps/skia
-cd ~/deps/dkia
-sudo wget https://github.com/aseprite/skia/releases/download/m81-b607b32047/Skia-Linux-Release-x64.zip
-sudo unzip *zip
-
-
 sudo apt-get install -y g++ cmake ninja-build libx11-dev libxcursor-dev libxi-dev libgl1-mesa-dev libfontconfig1-dev
 
-
-cd ~
 sudo git clone --recursive https://github.com/aseprite/aseprite.git
-
-sudo mkdir build
-
+cd aseprite
+mkdir build
 cd build
 
+sudo wget https://github.com/aseprite/skia/releases/download/m81-b607b32047/Skia-Linux-Release-x64.zip
+mkdir ./skia
+cd ./skia
+sudo unzip *zip
+cd ../
 
-sudo cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DLAF_BACKEND=skia -DSKIA_DIR=$HOME/deps/skia -DSKIA_LIBRARY_DIR=$HOME/deps/skia/out/Release-x64 -DSKIA_LIBRARY=$HOME/deps/skia/out/Release-x64/libskia.a -G Ninja ..
+
+sudo cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DLAF_BACKEND=skia -DSKIA_DIR=./skia -DSKIA_LIBRARY_DIR=./skia/out/Release-x64 -DSKIA_LIBRARY=./skia/out/Release-x64/libskia.a -G Ninja ..
 
 sudo ninja aseprite
+
+sudo strip bin/aseprite
+
+tar -acf /tmp/aseprite.tar aseprite
--- build.sh
+++ build.sh
@@ -1,20 +1,9 @@
 #!/usr/bin/bash
 
-aaa()
-{
-    git clone https://github.com/PaddiM8/kalk
-cd kalk/kalk_cli
-cargo install kalk_cli
-ls ~/.cargo/bin
-}
+
+aseprite () {
+        bash aseprite.sh
+    }
 
 
-bbb(){
-    git clone https://github.com/pkolaczk/fclones
-    cd fclones
-    cargo build --release --target=./
-    ls target
-}
-
-
-bbb
+aseprite
