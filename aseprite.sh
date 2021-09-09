c59634e0a14cb7fb542569e5a53c6eb82c6aafd4
TK4E <tk4ecc@gmail.com>
2021-09-09 11:09:37 +0800

    2021-09-09

--- aseprite.sh
+++ aseprite.sh
@@ -1,11 +1,11 @@
 sudo apt-get install -y g++ cmake ninja-build libx11-dev libxcursor-dev libxi-dev libgl1-mesa-dev libfontconfig1-dev
 
-sudo git clone --recursive https://github.com/aseprite/aseprite.git
+sudo git clone --recursive --depth 1 https://github.com/aseprite/aseprite.git
 cd aseprite
-mkdir build
+sudo mkdir build
 cd build
 
-mkdir ./skia
+sudo mkdir ./skia
 cd ./skia
 sudo wget https://github.com/aseprite/skia/releases/download/m81-b607b32047/Skia-Linux-Release-x64.zip
 sudo unzip *zip
