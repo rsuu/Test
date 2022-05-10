e52e7cc17dcf817bea682ecf3634cf5689afea75
TK4E <tk4ecc@gmail.com>
2022-05-10 10:06:51 +0800

    f(f):fff
    Signed-off-by: TK4E <tk4ecc@gmail.com>

--- build/ikemem.sh
+++ build/ikemem.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-sudo apt install golang-go git libgl1-mesa-dev xorg-dev libopenal1 libopenal-dev
+sudo apt install -y golang-go git libgl1-mesa-dev xorg-dev libopenal1 libopenal-dev
 
 git clone https://github.com/Windblade-GR01/Ikemen-GO.git
 
-cd Ikemen-GO
-
-make Ikemen_GO_Linux
+cd Ikemen-GO && make Ikemen_GO_Linux
 
 #cp font.ttf /usr/share/fonts/arial.ttf
 #MESA_GL_VERSION_OVERRIDE=4.4 ./Ikemen_GO_Linux -r mugen1
